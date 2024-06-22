ee436273c6d05b6c382c3d344bb9f71eeafb8684
Kirillov_Ivan <kirillovio@mail.ru>
2024-06-22 14:01:33 +0300

    настройка приложений

--- config/settings.py
+++ config/settings.py
@@ -76,8 +76,10 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "homework_20_1",
+        "USER": "postgres",
+        "PASSWORD": "12345",
     }
 }
 
@@ -123,3 +125,6 @@ STATICFILES_DIRS = (BASE_DIR / 'static',)
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
