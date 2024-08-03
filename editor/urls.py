6b67dd83921cd7ee918dc1b5011b23825f12889f
Kirillov_Ivan <kirillovio@mail.ru>
2024-08-03 13:40:26 +0300

    Настроил промотр списка статей и создание статей

--- editor/urls.py
+++ editor/urls.py
@@ -1,14 +1,14 @@
 from django.urls import path
 from editor.apps import EditorConfig
-from editor.views import EditorCreateView
+from editor.views import EditorCreateView, EditorListView, EditorDetailView
 app_name = EditorConfig.name
 
 
 
 urlpatterns = [
-   # path('', ...., name='list'),
+    path('', EditorListView.as_view(), name='list'),
     path('create/',EditorCreateView.as_view(), name='create'),
-  #  path('view/<int:pk>', ..., name='view'),
+    path('view/<int:pk>', EditorDetailView.as_view(), name='view'),
    # path('edit/<int:pk>', ..., name='edit'),
    # path('delete/<int:pk>', ..., name='delete')
 ]
--- editor/views.py
+++ editor/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.shortcuts import render, get_object_or_404
+from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView
 
 
@@ -11,7 +12,13 @@ from editor.models import Publications
 class EditorCreateView(CreateView):
     model = Publications
     fields = ('title', 'preview', 'body')
-    #template_name = 'editor/blog.html'
+    success_url = reverse_lazy('editor:list')
+
 
 class EditorListView(ListView):
     model = Publications
+
+
+class EditorDetailView(DetailView):
+    model = Publications
+    template_name = 'editor/publications_detail.html'
