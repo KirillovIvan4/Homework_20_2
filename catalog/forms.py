3d65623e8a97f1f1f01447f504f8f1411bfb4458
Kirillov_Ivan <kirillovio@mail.ru>
2024-08-10 18:58:16 +0300

    Настроил валидацию

--- catalog/forms.py
+++ catalog/forms.py
@@ -1,9 +1,20 @@
-from django.forms import ModelForm
+from django.forms import ModelForm, forms
 
 from catalog.models import Product
 
 
+ban_list = ['казино', 'криптовалюта', 'крипта', 'биржа', 'дешево', 'бесплатно', 'обман', 'полиция', 'радар']
 class ProductForm(ModelForm):
     class Meta:
         model = Product
         fields = ("__all__")
+
+    def clean_name(self):
+        cleaned_data = self.cleaned_data["name"]
+
+        if cleaned_data in ban_list:
+            for word in ban_list:
+                if word in cleaned_data.lower():
+                    raise forms.ValidationError(f"В названии продукта нельзя использовать запрещенные слово {word}.")
+
+        return cleaned_data
