7475843d412721349d06e8d7249e46d194374a15
Kirillov_Ivan <kirillovio@mail.ru>
2024-08-10 15:38:01 +0300

    Старт

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from catalog.apps import CatalogConfig
-from catalog.views import contacts, ProductListView, ProducDetailView
+from catalog.views import contacts, ProductListView, ProducDetailView, ProductUpdateView, ProductDeleteView
+
 app_name = CatalogConfig.name
 
 
@@ -8,5 +9,7 @@ app_name = CatalogConfig.name
 urlpatterns = [
     path('', ProductListView.as_view(), name='home'),
     path('contacts/', contacts, name='contacts'),
-    path('product/<int:pk>', ProducDetailView.as_view(), name='product')
+    path('view/<int:pk>', ProducDetailView.as_view(), name='view'),
+    path('edit/<int:pk>', ProductUpdateView.as_view(), name='edit'),
+    path('delete/<int:pk>', ProductDeleteView.as_view(), name='delete'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -23,10 +23,29 @@ class ProducDetailView(DetailView):
 
 class ProductCreateView(CreateView):
     model = Product
-    fields = ('title', 'preview', 'body')
+    fields = ('name','description', 'preview', 'category', 'purchase_price')
     success_url = reverse_lazy('product:list')
+
+    def form_valid(self, form):
+        new_mat = form.save()
+        new_mat.slug = slugify(new_mat.title)
+        new_mat.save()
+        return super().form_valid(form)
+
 class ProductUpdateView(UpdateView):
     model = Product
+    fields = ('name', 'description', 'preview', 'category', 'purchase_price')
 
+    def form_valid(self, form):
+        new_mat = form.save()
+        new_mat.slug = slugify(new_mat.title)
+        new_mat.save()
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse('editor:view', args=[self.kwargs.get('pk')])
 class ProductDeleteView(DeleteView):
     model = Product
+
+
+    success_url = reverse_lazy('editor:list')
