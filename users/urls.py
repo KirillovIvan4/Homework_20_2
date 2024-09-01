515b1e382078844a1ac508b46ef733856a2eecea
Kirillov_Ivan <kirillovio@mail.ru>
2024-09-01 06:59:18 +0300

    настроил вход и выход для пользователя

---
+++ users/urls.py
@@ -0,0 +1,15 @@
+from django.contrib.auth.views import LoginView, LogoutView
+
+from catalog.apps import CatalogConfig
+from users.apps import UsersConfig
+from django.urls import path
+
+app_name = UsersConfig.name
+
+
+
+urlpatterns = [
+    path('login/', LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+]
+
